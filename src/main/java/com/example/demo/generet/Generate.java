2a4dcb13b946e68d325c9170082dc0731093db52
hafizsritonga08@gmail.com <116991026+191110538@users.noreply.github.com>
2023-08-07 00:26:57 +0700

    pembaruan_usulan

--- src/main/java/com/example/demo/generet/Generate.java
+++ src/main/java/com/example/demo/generet/Generate.java
@@ -97,77 +97,76 @@ public class Generate {
     }
 
     @PutMapping("/usulan/{id}")
-    public ResponseEntity<?> updateUsulanKegiatan(@PathVariable Long id, @RequestBody Usulan usulanData) {
+    public ResponseEntity<?> updateUsulanKegiatan(@PathVariable Long id, @RequestBody Usulan updatedUsulan) {
         // Cari data usulan berdasarkan ID
         Optional<Usulan> usulanOptional = usulanRepository.findById(id);
 
         if (usulanOptional.isPresent()) {
             Usulan usulan = usulanOptional.get();
-
             // Update data usulan dengan data dari updatedUsulan
-            usulan.setUser(usulanData.getUser());
-            usulan.setOrmawa(usulanData.getOrmawa());
-            usulan.setRevisi_usulan(usulanData.getRevisi_usulan());
-            usulan.setPembiayaan(usulanData.getPembiayaan());
-            usulan.setNama_kegiatan(usulanData.getNama_kegiatan());
-            usulan.setBentuk_kegiatan(usulanData.getBentuk_kegiatan());
-            usulan.setKategori_bentuk_kegiatan(usulanData.getKategori_bentuk_kegiatan());
-            usulan.setDeskripsi_kegiatan(usulanData.getDeskripsi_kegiatan());
-            usulan.setTanggal_mulai_kegiatan(usulanData.getTanggal_mulai_kegiatan());
-            usulan.setTanggal_selesai_kegiatan(usulanData.getTanggal_selesai_kegiatan());
-            usulan.setWaktu_mulai_kegiatan(usulanData.getWaktu_mulai_kegiatan());
-            usulan.setWaktu_selesai_kegiatan(usulanData.getWaktu_selesai_kegiatan());
-            usulan.setTempat_kegiatan(usulanData.getTempat_kegiatan());
-            usulan.setTanggal_keberangkatan(usulanData.getTanggal_keberangkatan());
-            usulan.setTanggal_kepulangan(usulanData.getTanggal_kepulangan());
-            usulan.setJumlah_partisipan(usulanData.getJumlah_partisipan());
-            usulan.setKategori_jumlah_partisipan(usulanData.getKategori_jumlah_partisipan());
-            usulan.setTarget_kegiatan(usulanData.getTarget_kegiatan());
-            usulan.setTotal_pendanaan(usulanData.getTotal_pendanaan());
-            usulan.setKategori_total_pendanaan(usulanData.getKategori_total_pendanaan());
-            usulan.setKeterangan(usulanData.getKeterangan());
-            usulan.setTanda_tangan_ormawa(usulanData.getTanda_tangan_ormawa());
-            usulan.setPartisipan(usulanData.getPartisipan());
-            usulan.setBiaya_kegiatan(usulanData.getBiaya_kegiatan());
-            usulan.setLatar_belakang(usulanData.getLatar_belakang());
-            usulan.setTujuan_kegiatan(usulanData.getTujuan_kegiatan());
-            usulan.setManfaat_kegiatan(usulanData.getManfaat_kegiatan());
-            usulan.setBentuk_pelaksanaan_kegiatan(usulanData.getBentuk_pelaksanaan_kegiatan());
-            usulan.setTarget_pencapaian_kegiatan(usulanData.getTarget_pencapaian_kegiatan());
-            usulan.setWaktu_dan_tempat_pelaksanaan(usulanData.getWaktu_dan_tempat_pelaksanaan());
-            usulan.setRencana_anggaran_kegiatan(usulanData.getRencana_anggaran_kegiatan());
-            usulan.setTotal_biaya(usulanData.getTotal_biaya());
-            usulan.setTertib_acara(usulanData.getTertib_acara());
-            usulan.setPerlengkapan_dan_peralatan(usulanData.getPerlengkapan_dan_peralatan());
-            usulan.setPenutup(usulanData.getPenutup());
-            usulan.setFoto_postingan_kegiatan(usulanData.getFoto_postingan_kegiatan());
-            usulan.setFoto_surat_undangan_kegiatan(usulanData.getFoto_surat_undangan_kegiatan());
-            usulan.setFoto_linimasa_kegiatan(usulanData.getFoto_linimasa_kegiatan());
-            usulan.setFoto_tempat_kegiatan(usulanData.getFoto_tempat_kegiatan());
-            usulan.setFile_usulan_kegiatan(usulanData.getFile_usulan_kegiatan());
-            usulan.setValidasi_pembina(usulanData.getValidasi_pembina());
-            usulan.setTanda_tangan_pembina(usulanData.getTanda_tangan_pembina());
-            usulan.setStatus_usulan(usulanData.getStatus_usulan());
-            usulan.setRoles(usulanData.getRoles());
-            usulan.setCreated_at(usulanData.getCreated_at());
-            usulan.setUpdated_at(usulanData.getUpdated_at());
-            usulan.setCreated_by(usulanData.getCreated_by());
-            usulan.setUpdated_by(usulanData.getUpdated_by());
+            usulan.setUser(updatedUsulan.getUser());
+            usulan.setOrmawa(updatedUsulan.getOrmawa());
+            usulan.setRevisi_usulan(updatedUsulan.getRevisi_usulan());
+            usulan.setPembiayaan(updatedUsulan.getPembiayaan());
+            usulan.setNama_kegiatan(updatedUsulan.getNama_kegiatan());
+            usulan.setBentuk_kegiatan(updatedUsulan.getBentuk_kegiatan());
+            usulan.setKategori_bentuk_kegiatan(updatedUsulan.getKategori_bentuk_kegiatan());
+            usulan.setDeskripsi_kegiatan(updatedUsulan.getDeskripsi_kegiatan());
+            usulan.setTanggal_mulai_kegiatan(updatedUsulan.getTanggal_mulai_kegiatan());
+            usulan.setTanggal_selesai_kegiatan(updatedUsulan.getTanggal_selesai_kegiatan());
+            usulan.setWaktu_mulai_kegiatan(updatedUsulan.getWaktu_mulai_kegiatan());
+            usulan.setWaktu_selesai_kegiatan(updatedUsulan.getWaktu_selesai_kegiatan());
+            usulan.setTempat_kegiatan(updatedUsulan.getTempat_kegiatan());
+            usulan.setTanggal_keberangkatan(updatedUsulan.getTanggal_keberangkatan());
+            usulan.setTanggal_kepulangan(updatedUsulan.getTanggal_kepulangan());
+            usulan.setJumlah_partisipan(updatedUsulan.getJumlah_partisipan());
+            usulan.setKategori_jumlah_partisipan(updatedUsulan.getKategori_jumlah_partisipan());
+            usulan.setTarget_kegiatan(updatedUsulan.getTarget_kegiatan());
+            usulan.setTotal_pendanaan(updatedUsulan.getTotal_pendanaan());
+            usulan.setKategori_total_pendanaan(updatedUsulan.getKategori_total_pendanaan());
+            usulan.setKeterangan(updatedUsulan.getKeterangan());
+            usulan.setTanda_tangan_ormawa(updatedUsulan.getTanda_tangan_ormawa());
+            usulan.setPartisipan(updatedUsulan.getPartisipan());
+            usulan.setBiaya_kegiatan(updatedUsulan.getBiaya_kegiatan());
+            usulan.setLatar_belakang(updatedUsulan.getLatar_belakang());
+            usulan.setTujuan_kegiatan(updatedUsulan.getTujuan_kegiatan());
+            usulan.setManfaat_kegiatan(updatedUsulan.getManfaat_kegiatan());
+            usulan.setBentuk_pelaksanaan_kegiatan(updatedUsulan.getBentuk_pelaksanaan_kegiatan());
+            usulan.setTarget_pencapaian_kegiatan(updatedUsulan.getTarget_pencapaian_kegiatan());
+            usulan.setWaktu_dan_tempat_pelaksanaan(updatedUsulan.getWaktu_dan_tempat_pelaksanaan());
+            usulan.setRencana_anggaran_kegiatan(updatedUsulan.getRencana_anggaran_kegiatan());
+            usulan.setTotal_biaya(updatedUsulan.getTotal_biaya());
+            usulan.setTertib_acara(updatedUsulan.getTertib_acara());
+            usulan.setPerlengkapan_dan_peralatan(updatedUsulan.getPerlengkapan_dan_peralatan());
+            usulan.setPenutup(updatedUsulan.getPenutup());
+            usulan.setFoto_postingan_kegiatan(updatedUsulan.getFoto_postingan_kegiatan());
+            usulan.setFoto_surat_undangan_kegiatan(updatedUsulan.getFoto_surat_undangan_kegiatan());
+            usulan.setFoto_linimasa_kegiatan(updatedUsulan.getFoto_linimasa_kegiatan());
+            usulan.setFoto_tempat_kegiatan(updatedUsulan.getFoto_tempat_kegiatan());
+            usulan.setFile_usulan_kegiatan(updatedUsulan.getFile_usulan_kegiatan());
+            usulan.setValidasi_pembina(updatedUsulan.getValidasi_pembina());
+            usulan.setTanda_tangan_pembina(updatedUsulan.getTanda_tangan_pembina());
+            usulan.setStatus_usulan(updatedUsulan.getStatus_usulan());
+            usulan.setRoles(updatedUsulan.getRoles());
+            usulan.setCreated_at(updatedUsulan.getCreated_at());
+            usulan.setUpdated_at(updatedUsulan.getUpdated_at());
+            usulan.setCreated_by(updatedUsulan.getCreated_by());
+            usulan.setUpdated_by(updatedUsulan.getUpdated_by());
             // Lanjutkan dengan mengupdate properti lain sesuai kebutuhan
 
             // Simpan data usulan yang telah diupdate ke database
             usulanRepository.save(usulan);
 
-            // Jika semua properti telah terisi, baru jalankan generate dan POST ke server
-            if (isAllPropertiesFilled(usulan)) {
-                // Generate file docx dan dapatkan URL file docx
+            // Check conditions for generating the docx file
+            if (updatedUsulan.getRevisi_usulan() == null && !updatedUsulan.getPenutup().isEmpty()
+                    || "Disetujui".equals(updatedUsulan.getValidasi_pembina())) {
+                // Generate file docx and get the URL file docx
                 String fileUrl = generateDocx(usulan);
 
                 // Set properti "file_usulan_kegiatan" dengan nilai fileUrl
                 usulan.setFile_usulan_kegiatan(fileUrl);
 
-                // Update kembali data usulan dengan properti "file_usulan_kegiatan" yang telah
-                // terisi
+                // Update kembali data usulan dengan properti "file_usulan_kegiatan" yang telah terisi
                 usulanRepository.save(usulan);
 
                 // Tampilkan URL file docx yang telah disimpan
